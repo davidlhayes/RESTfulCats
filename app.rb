2cfd28799ae7cd66ebbcc105a3ffe72cfa439fa6
David Hayes <davidlynnhayes@att.net>
2015-08-17 15:47:47 -0500

    final restful api

--- app.rb
+++ app.rb
@@ -5,3 +5,58 @@ ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
   :database => 'restfulcats'
 )
+
+# does our model and sinatra work? let's see...
+get '/' do
+  @cat = Cat.new
+  @cat.name = 'Bidala'
+  @cat.breed = 'persian'
+  @cat.save
+  cat.to_json
+end
+
+get '/api/cats' do
+
+  Cat.all.to_json
+
+end
+
+get '/api/cats/:id' do
+
+  puts params
+  Cat.find(params[:id]).to_json
+
+end
+
+post '/api/cats' do
+    puts params
+    Cat.create({
+      :name => params[:name],
+      :breed => params[:breed]
+      }).to_json
+end
+
+# PATCH: Update by ID (NEVER update the ID of an existing record)
+
+patch '/api/cats/:id' do
+  cat_args = { :name => params[:name], :breed => params[:breed] }
+
+  @cat = Cat.find(params[:id])
+  @cat.update(cat_args)
+  @cat.to_json
+
+end
+
+# put or patch not supported everywhere
+put '/api/cats/:id' do
+  cat_args = { :name => params[:name], :breed => params[:breed] }
+
+  @cat = Cat.find(params[:id])
+  @cat.update(cat_args)
+  @cat.to_json
+
+end
+
+delete '/api/cats/:id' do
+  Cat.destroy(params[:id]).to_json
+end
